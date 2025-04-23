810024d5bc7b4c8d37ea58739bce16daf8c105e9
seilah <princenoru@gmail.com>
2025-04-23 17:05:40 +0200

    main: for -> while + simplify return

--- homework.js
+++ homework.js
@@ -1,17 +1,24 @@
-let result = sumDigits(8404);
+let result = sumDigits(12341);
 console.log(`sum of digits = ${result}`); // 1234 -> 10
 
 
-result = luckyNumber(8744054); // 123871 --> 1+3+7 === 2+8+1 (11 === 11)
-console.log(result ? 'luckyNumber' : 'unluckyNumber');
+result = luckyNumber(123871); // 123871 --> 1+3+7 === 2+8+1 (11 === 11)
+console.log(result ? 'luckyNumber' : 'UN_luckyNumber');
 
 function sumDigits(num) {
     let sum = 0;
-    let numLength = num.toString().length
-    for (let i in numLength) {
+    let numLength = num.toString().length;
+    for (let i = 0; i < numLength; i++) {
         let lastDigit = num % 10;
         sum += lastDigit;
         num = Math.trunc(num / 10);
+    }
+    let i = 0;
+    while (i < numLength) {
+        let lastDigit = num % 10;
+        sum += lastDigit;
+        num = Math.trunc(num / 10);
+        numLength--;
     }
     return sum;
 }
@@ -21,14 +28,15 @@ function luckyNumber(givenNumber) {
     let digitsOnOddInd = "";
 
     let numberToString = givenNumber.toString();
-    let numberLength = numberToString.length;
+    const numberLength = numberToString.length;
 
-    for (let i in numberLength) {
+    for (let i = 0; i < numberLength; i++) {
         if (i % 2 === 0) {
             digitsOnEvenInd += numberToString[i];
         } else {
             digitsOnOddInd += numberToString[i];
         }
     }
-    return sumDigits(digitsOnEvenInd) === sumDigits(digitsOnOddInd)
+
+    return sumDigits(digitsOnEvenInd) === sumDigits(digitsOnOddInd);
 }
