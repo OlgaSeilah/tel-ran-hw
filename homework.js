9aeb93b3d097dfb2ccc04552095d4a79b54eff71
seilah <princenoru@gmail.com>
2025-06-14 19:03:10 +0200

    hw-22-scopes: hw 22 - fixing scope mistake

--- homework.js
+++ homework.js
@@ -1,60 +1,22 @@
-// ---------- Original ----------
-let group = {
-    title: 'Java 61',
-    students: ['Adassa', 'Alisa', 'Anna', 'Irina', 'Maria', 'Nadezhda', 'Olga', 'Sofia',
-        'Tamara', 'Tatiana', 'Tatyana', 'Seilah', 'Julia'],
-    showList: function () {
-        const show = function (name) {
-            console.log(`${this.title}: ${name}`)
-        }
-        this.students.forEach(show);
+// Problem -> console 10 times 10
+// for (var i = 0; i < 10; i++) {
+//     setTimeout(function() {
+//         console.log(i);
+//     }, 1000);
+// }
+
+// Solution 1 -> use let
+// for (let i = 0; i < 10; i++) {
+//     setTimeout(function() {
+//         console.log(i);
+//     }, 1000);
+// }
+
+// Solution 2 -> use function
+const func = function () {
+    for (var i = 0; i < 10; i++) {
+        console.log(i)
     }
 }
 
-// ---------- context loss -> fix by adding arrow func ----------
-let group = {
-    title: 'Java 61',
-    students: ['Adassa', 'Alisa', 'Anna', 'Irina', 'Maria', 'Nadezhda', 'Olga', 'Sofia',
-        'Tamara', 'Tatiana', 'Tatyana', 'Seilah', 'Julia'],
-    showList: function ()  {
-        const show =  (name) => {
-            console.log(`${this.title}: ${name}`)
-        }
-        this.students.forEach(show);
-    }
-}
-
-// ---------- context loss -> fix by adding context at current point where it is lost ----------
-let group = {
-    title: 'Java 61',
-    students: ['Adassa', 'Alisa', 'Anna', 'Irina', 'Maria', 'Nadezhda', 'Olga', 'Sofia',
-        'Tamara', 'Tatiana', 'Tatyana', 'Seilah', 'Julia'],
-    showList: function () {
-        let self = this;
-        console.log(self)
-        const show = function (name) {
-            console.log(`${this.title}: ${name}`)
-            console.log(self)
-        }
-        this.students.forEach(show.bind(this));
-    }
-}
-
-// ---------- context loss -> fix by using one func showList instead of using showList + show ----------
-let group = {
-    title: 'Java 61',
-    students: ['Adassa', 'Alisa', 'Anna', 'Irina', 'Maria', 'Nadezhda', 'Olga', 'Sofia',
-        'Tamara', 'Tatiana', 'Tatyana', 'Seilah', 'Julia'],
-    showList: function () {
-        this.students.forEach(student => {
-            console.log(`${this.title}: ${student}`);})
-    }
-}
-
-
-// Unmodified
-group.showList();
-const newGroup = group;
-group = null;
-console.log('==========');
-newGroup.showList();
+func();
